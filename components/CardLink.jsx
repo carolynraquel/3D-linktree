11e19f7ed5d7325dc4111f9ba6ee321ee38e464e
Carolyn Carney <50558142+carolynraquel@users.noreply.github.com>
2024-12-05 16:46:48 -0600

    Cardlink click workaround

--- components/CardLink.jsx
+++ components/CardLink.jsx
@@ -1,15 +1,16 @@
 "use client";
 export default function CardLink({ link }) {
-    function goTo(goToLink) {
-        window.open(goTolink, "_blank");
-    }
+  const handleClick = () => {
+    window.open(link.link, "_blank");
+  };
 
-    return (
-        <div 
-            onClick={() => goTo(link.link)} 
-            className="bg-white flex items-center mx-4 max-w-md w-full rounded-full p-4 shadow-lg shadow-violet-500 cursor-pointer">
-            {link.icon}
-            <p className="font-bold m-auto">{link.text}</p>
-        </div>
-    );
+  return (
+    <div
+      onClick={handleClick}
+      className="bg-white flex items-center mx-4 max-w-md w-full rounded-full p-4 shadow-lg shadow-violet-500 cursor-pointer"
+    >
+      {link.icon}
+      <p className="font-bold m-auto">{link.text}</p>
+    </div>
+  );
 }

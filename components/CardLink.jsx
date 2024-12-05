a413cedea6f77648078554291b1a5861ef43ff6f
Carolyn Carney <50558142+carolynraquel@users.noreply.github.com>
2024-12-05 16:56:41 -0600

    add comments to cardlink.jsx

--- components/CardLink.jsx
+++ components/CardLink.jsx
@@ -1,4 +1,6 @@
 "use client";
+
+// go to link on button click
 export default function CardLink({ link }) {
   const handleClick = () => {
     window.open(link.link, "_blank");
@@ -7,9 +9,14 @@ export default function CardLink({ link }) {
   return (
     <div
       onClick={handleClick}
+      
+    //   style buttons
       className="bg-white flex items-center mx-4 max-w-md w-full rounded-full p-4 shadow-lg shadow-violet-500 cursor-pointer"
     >
       {link.icon}
+
+    {/* style fonts */}
+
       <p className="font-bold m-auto">{link.text}</p>
     </div>
   );

4ed75fa7071dd838ea72e5bc4c46a3189b8783e2
Carolyn Carney <50558142+carolynraquel@users.noreply.github.com>
2024-12-05 15:44:32 -0600

    finish styling Cardlink

--- components/CardLink.jsx
+++ components/CardLink.jsx
@@ -1,6 +1,6 @@
 export default function CardLink({ link }) {
   return (
-    <div className="bg-white flex items-center">
+    <div className="bg-white flex items-center mx-4 max-w-md w-full rounded-full p-4 shadow-lg shadow-violet-500 cursor-pointer">
         {link.icon}
         <p>{link.text}</p>
     </div>

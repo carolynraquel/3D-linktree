09c30c48926ec98e5fef9c88355147343e65e5b4
Carolyn Carney <50558142+carolynraquel@users.noreply.github.com>
2024-12-05 16:32:17 -0600

    format CardLink component for improved readability

--- components/CardLink.jsx
+++ components/CardLink.jsx
@@ -5,7 +5,9 @@ export default function CardLink({ link }) {
     }
 
     return (
-        <div onClick={() => goTo(link.link)} className="bg-white flex items-center mx-4 max-w-md w-full rounded-full p-4 shadow-lg shadow-violet-500 cursor-pointer">
+        <div 
+            onClick={() => goTo(link.link)} 
+            className="bg-white flex items-center mx-4 max-w-md w-full rounded-full p-4 shadow-lg shadow-violet-500 cursor-pointer">
             {link.icon}
             <p className="font-bold m-auto">{link.text}</p>
         </div>

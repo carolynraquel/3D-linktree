dc60a2503c5d0c1068b48aec25825207b69afa42
Carolyn Carney <50558142+carolynraquel@users.noreply.github.com>
2024-12-05 16:18:51 -0600

    import social media dependencies and add goTo function to CardLink

--- components/CardLink.jsx
+++ components/CardLink.jsx
@@ -1,8 +1,13 @@
+"use client";
 export default function CardLink({ link }) {
-  return (
-    <div onClick={} className="bg-white flex items-center mx-4 max-w-md w-full rounded-full p-4 shadow-lg shadow-violet-500 cursor-pointer">
-        {link.icon}
-        <p className="font-bold m-auto">{link.text}</p>
-    </div>
-  );
+    function goTo(goToLink) {
+        window.open(goTolink);
+    }
+
+    return (
+        <div onClick={() => goTo(link.link)} className="bg-white flex items-center mx-4 max-w-md w-full rounded-full p-4 shadow-lg shadow-violet-500 cursor-pointer">
+            {link.icon}
+            <p className="font-bold m-auto">{link.text}</p>
+        </div>
+    );
 }

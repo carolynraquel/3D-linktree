d863f5e44625bcbd3ea9f026361e8c55c2524848
chirix33 <chirixteam@gmail.com>
2024-12-05 11:52:16 -0600

    Move image to public directory

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 export default function Avatar() {
-    return <Image src='../assets/Images/logo.jpg' alt="Brown-skinned woman with large natural afro holds fist in air. She is wearing a reflective black trench coat. Her eyes are closed and her face tilts up toward blush pink lights." 
+    return <Image src='/logo.jpg' alt="Brown-skinned woman with large natural afro holds fist in air. She is wearing a reflective black trench coat. Her eyes are closed and her face tilts up toward blush pink lights." 
     width={100} 
     height={100}
     className="w-24 h-24 rounded-full"

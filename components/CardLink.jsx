c4bf875a23f31295ec22c3d466427b0dce98f728
Carolyn Carney <50558142+carolynraquel@users.noreply.github.com>
2024-12-05 18:36:14 -0600

    animated buttons

--- components/CardLink.jsx
+++ components/CardLink.jsx
@@ -1,9 +1,17 @@
 "use client";
 
+import {useState} from "react";
+
 // go to link on button click
 export default function CardLink({ link }) {
+  const [startAnimation, setStartAnimation] = useState(false);
+  
   const handleClick = () => {
-    window.open(link.link, "_blank");
+    setStartAnimation(true);
+    setTimeout(() => {
+        window.open(link.link, "_blank");
+    }, 1050);
+    
   };
 
   return (
@@ -11,13 +19,28 @@ export default function CardLink({ link }) {
       onClick={handleClick}
       
     //   style buttons
-      className="bg-white flex items-center mx-4 max-w-md w-full rounded-full p-4 shadow-lg shadow-violet-500 cursor-pointer"
-    >
+      className="bg-white flex items-center mx-4 max-w-md w-full h-[5.5rem] rounded-full p-4 shadow-lg shadow-violet-500 cursor-pointer overflow-hidden relative hover:shadow-shmedium hover:shadow-violet-600 duration-[0.2s] CardLink"
+    > 
+    <div className={`absolute top-0 left-0 w-full h-full flex items-center justify-center CardLinkCircle1 ${startAnimation ? "CardLinkCircle1Animation" : ""}`}>
+
+        <div className="w-[2.5rem] h-[2.5rem] rounded-full bg-purple-500 z-10">
+
+        </div>
+        
+    </div>
+    <div className={`absolute top-0 left-0 w-full h-full flex items-center justify-center CardLinkCircle2 ${startAnimation ? "CardLinkCircle2Animation" : ""}`}>
+        <div className="w-[1rem] h-[1rem] rounded-full bg-purple-300 z-10">
+
+        </div>
+        
+    </div>
+      <div className="flex items-center w-full bg-[transparent] absolute z-20 h-full p-4"> 
       {link.icon}
 
-    {/* style fonts */}
+{/* style fonts */}
 
-      <p className="font-bold m-auto">{link.text}</p>
+  <p className="font-bold m-auto">{link.text}</p>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      boxShadow: {
+        "shmedium": "0 12px 16px -4px var(--tw-shadow-color), 0 6px 8px -6px var(--tw-shadow-color)"
+      }
     },
   },
   plugins: [],
